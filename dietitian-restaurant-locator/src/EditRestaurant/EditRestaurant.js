6f4329c3a8f2727bf9e4d8834a01213e3b76ff47
yl1462 <yliang1462@hotmail.com>
2021-05-01 22:03:07 -0500

    fixed font

--- dietitian-restaurant-locator/src/EditRestaurant/EditRestaurant.js
+++ dietitian-restaurant-locator/src/EditRestaurant/EditRestaurant.js
@@ -33,24 +33,24 @@ class EditRestaurant extends Component {
       the_restaurant: this.state.the_restaurant,
       type: this.state.type,
       id: this.state.id
-  }
-    
+    }
+
     fetch(`${config.API_ENDPOINT}/api/restaurant/${this.state.id}`, {
-        method:'PATCH',
-        headers:{
-            'Content-Type':'application/json'
-        },
-        body:JSON.stringify(editedRestaurant)
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(editedRestaurant)
     })
-    .then(() => {
-      console.log('edit')
-      this.props.editRestaurant(editedRestaurant, this.state.index)
-      this.props.history.push('/home')
-    })
-    .catch(err => {
-        this.setState({err})
-    })
-}
+      .then(() => {
+        console.log('edit')
+        this.props.editRestaurant(editedRestaurant, this.state.index)
+        this.props.history.push('/home')
+      })
+      .catch(err => {
+        this.setState({ err })
+      })
+  }
 
   render() {
     const { the_restaurant, type } = this.state
@@ -60,24 +60,32 @@ class EditRestaurant extends Component {
         {/* submit edited restaurant, with required name, and optional type */}
         <div className="EditRestaurant">
           <form onSubmit={this.handleSubmit}>
-            <input 
-              type='text' 
-              value={the_restaurant}  
+            <input
+              type='text'
+              value={the_restaurant}
               name='the_restaurant'
               onChange={this.handleChange}
               className="Placeholder"
               required
               min={3}
-              /><br />
-            <select required onChange={this.handleChange} name='type' value={type} >
+            />
+            <br />
+            <select
+              required
+              onChange={this.handleChange}
+              name='type'
+              className="Placeholder"
+              type='selected'
+              value={type}
+            >
               <option value='Keto'>Keto</option>
               <option value='Mediterranean'>Mediterranean</option>
               <option value='Plant-based'>Plant-based</option>
             </select>
             <br />
-            <button 
-              type='submit' 
-              >Update</button>
+            <button
+              type='submit'
+            >Update</button>
           </form>
         </div>
       </>
